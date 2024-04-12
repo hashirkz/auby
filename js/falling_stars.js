3c7374c5980921995959610a34e15fb956bbfce2
hashir <75959275+zzsleepyzzz@users.noreply.github.com>
2024-04-11 21:45:54 -0400

    initial commit

---
+++ js/falling_stars.js
@@ -0,0 +1,9 @@
+// need to add src to anime.js in html
+// find random x pos to start
+
+let random_pos = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+
+
+function falling_stars(speed=0.5) {
+    
+}
